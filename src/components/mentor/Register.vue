288c5312bc4f1bd2d90f4ec7aae119c72b7845ee
Lazurite259 <hsiavivi@gmail.com>
2022-11-03 03:52:09 -0700

    integrate with backend

--- src/components/mentor/Register.vue
+++ src/components/mentor/Register.vue
@@ -5,26 +5,27 @@
             <form @submit.prevent="handleSubmitForm">
                 <div class="form-group">
                     <label>First Name</Label>
-                    <input type="text" class="form-control" v-model="user.first_name" placeholder="First Name"
+                    <input type="text" class="form-control" v-model="mentor.first_name" placeholder="First Name"
                         required />
                 </div>
                 <div class="
                 form-group">
                     <label>Last Name</Label>
-                    <input type="text" class="form-control" v-model="user.last_name" placeholder="Last Name" required />
+                    <input type="text" class="form-control" v-model="mentor.last_name" placeholder="Last Name"
+                        required />
                 </div>
                 <div class="form-group">
                     <label>Email</Label>
-                    <input type="text" class="form-control" v-model="user.email" placeholder="Email" required />
+                    <input type="text" class="form-control" v-model="mentor.email" placeholder="Email" required />
                 </div>
                 <div class="form-group">
                     <label>Password</Label>
-                    <input type="password" class="form-control" v-model="user.password" placeholder="Password"
+                    <input type="password" class="form-control" v-model="mentor.password" placeholder="Password"
                         required />
                 </div>
                 <div class="form-group">
                     <label>Comfirm Password</Label>
-                    <input type="password" class="form-control" v-model="user.password_confirm"
+                    <input type="password" class="form-control" v-model="mentor.password_confirm"
                         placeholder="Comfirm Password" required />
                 </div>
                 <div class="form-group">
@@ -35,20 +36,34 @@
     </div>
 </template>
 <script>
+import axios from "axios";
 export default {
     data() {
         return {
-            user: {
+            mentor: {
                 first_name: '',
                 last_name: '',
                 email: '',
                 password: '',
-                password_confirm: '',
             }
         }
     },
     methods: {
-        handleSubmitForm() { }
+        handleSubmitForm() {
+            let apiURL = 'http://localhost:4000/api/create-mentor';
+
+            axios.post(apiURL, this.mentor).then(() => {
+                this.$router.push('/view')
+                this.mentor = {
+                    first_name: '',
+                    last_name: '',
+                    email: '',
+                    password: '',
+                }
+            }).catch(error => {
+                console.log(error)
+            });
+        }
     }
 }
 </script>
