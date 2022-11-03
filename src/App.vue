3e5824587fd06531632eebbdf4960315fc08d1d5
Lazurite259 <hsiavivi@gmail.com>
2022-11-03 16:13:10 -0700

    add dropdown navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- Nav bar -->
-    <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
+    <!-- <nav class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row">
       <div class="container">
         <router-link class="navbar-brand float-left" to="/">GetMentored</router-link>
         <ul class="nav navbar-nav flex-row float-right">
@@ -27,6 +27,42 @@
             <router-link class="nav-link" to="/view">Test</router-link>
           </li>
         </ul>
+      </div>
+    </nav> -->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid">
+        <router-link class="navbar-brand float-left" to="/">GetMentored</router-link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link class="nav-link pr-3" to="/career">Career Resources</router-link>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownSignUp" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Sign Up
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownSignUp">
+                <li><a class="dropdown-item" href="mentor-register">As Mentor</a></li>
+                <li><a class="dropdown-item" href="mentee-register">As Mentee</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownLogin" role="button"
+                data-bs-toggle="dropdown" aria-expanded="false">
+                Login
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownLogin">
+                <li><a class="dropdown-item" href="mentor-login">As Mentor</a></li>
+                <li><a class="dropdown-item" href="mentee-login">As Mentee</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
     <!-- Router view -->
@@ -55,5 +91,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.navbar {
+  background-color: #4B0082;
+}
+
+/* .navbar-nav .nav-link {
+  color: #fff;
+} */
+.navbar .navbar-nav .nav-item .nav-link {
+  color: #fff;
+}
+
+.navbar .navbar-brand {
+  color: #fff;
 }
 </style>
