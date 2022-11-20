398fb9b3d4c85e4e85f35c5096d4a9d1bfa08f15
Lazurite259 <hsiavivi@gmail.com>
2022-11-20 03:36:55 -0800

    add get and update profile (birth_date unfixed)

--- backend/routes/mentor.route.js
+++ backend/routes/mentor.route.js
@@ -4,23 +4,24 @@ const auth = require('../auth')
 // model
 const Mentor = require('../models/Mentor')
 // route
-app.get('/', async (req, res, next) => {
-  Mentor.find((error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      return res.json(data)
-    }
-  })
-})
+// app.get('/', async (req, res, next) => {
+//   Mentor.find((error, data) => {
+//     if (error) {
+//       return next(error)
+//     } else {
+//       return res.json(data)
+//     }
+//   })
+// })
 app.post('/mentor-register', async (req, res) => {
   try {
-    // console.log(isUser);
-    // if (isUser.length >= 1) {
-    //     return res.status(409).json({
-    //         message: "email already in use"
-    //     });
-    // }
+    const isUser = await Mentor.find({ email: req.body.email });
+    console.log(isUser);
+    if (isUser.length >= 1) {
+      return res.status(409).json({
+        message: 'Email already in use'
+      })
+    }
     const mentor = new Mentor({
       first_name: req.body.first_name,
       last_name: req.body.last_name,
@@ -41,32 +42,45 @@ app.post('/mentor-login', async (req, res) => {
     const password = req.body.password
     const mentor = await Mentor.findByCredentials(email, password)
     if (!mentor) {
-      return res.status(401).json({ error: 'Login failed! Check authentication credentials' })
+      return res.status(401).json({ message: 'Login failed! Check your email or password' })
     }
     const token = await mentor.generateAuthToken()
     res.status(201).json({ mentor, token })
-  } catch (err) {
-    res.status(400).json({ err })
+  } catch (error) {
+    res.status(400).json({ error: error })
   }
 })
 app.get('/mentor-profile/:id', auth, async (req, res) => {
-  // try{
-  //   const mentor = await Mentor.find({_id:`${req.params.id}`})
-  //   res.json(mentor)
-  // }catch (error){
-  //   res.status(500).send(error)
-  // }
-  Mentor.findById(req.params.id, (error, data) => {
-            if (error) {
-                return next(error)
-            } else {
-                res.json(data)
-            }
-        })
+  try{
+    const mentor = await Mentor.findById(req.params.id)
+    res.json(mentor)
+  }catch (error){
+    res.status(500).send(error)
+  }
 })
 
-app.get('/:id', async (req, res, next) => {
-  const mentors = await Mentor.find({ occupation_title: `${req.params.id}` })
+app.put('/mentor-update/:id', async (req, res) => {
+  try{
+    const mentor = await Mentor.findByIdAndUpdate(req.params.id, {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      occupation_title: req.body.occupation_title,
+      birth_date: req.body.birth_date,
+      company_name: req.body.company_name,
+      year_of_experience: req.body.year_of_experience,
+      office_hour: req.body.office_hour,
+      linkedin: req.body.linkedin,
+      introduction: req.body.introduction,
+    })
+    const token = await mentor.generateAuthToken()
+    res.status(201).json({ mentor, token })
+  }catch (error){
+    res.status(500).send(error)
+  }
+})
+
+app.get('/:career', async (req, res, next) => {
+  const mentors = await Mentor.find({ occupation_title: `${req.params.career}` })
   try {
     res.send(mentors)
   } catch (error) {
@@ -74,17 +88,6 @@ app.get('/:id', async (req, res, next) => {
   }
 })
 
-
-
-// mentorRoute.route('/edit-mentor/:id').get((req, res, next) => {
-//     MentorModel.findById(req.params.id, (error, data) => {
-//         if (error) {
-//             return next(error)
-//         } else {
-//             res.json(data)
-//         }
-//     })
-// })
 // // Update
 // mentorRoute.route('/update-mentor/:id').put((req, res, next) => {
 //     MentorModel.findByIdAndUpdate(req.params.id, {
