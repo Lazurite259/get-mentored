e1fdf2b5a5d81527a68c9876456f318e5e4b9b07
Lazurite259 <hsiavivi@gmail.com>
2022-11-22 16:59:33 -0800

    fix nodemailer

--- backend/sendEmail.js
+++ backend/sendEmail.js
@@ -1,27 +1,32 @@
 const nodemailer = require('nodemailer')
 
 const sendEmail = async (option) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASSWORD
+      },
+      tls: { ciphers: 'SSLv3' }
+    })
+    const message = {
+      from: `GetMentored <${process.env.SMTP_EMAIL}>`,
+      to: option.email,
+      subject: 'Reset GetMentored.com Password',
+      text: option.message
     }
-  })
-  const message = {
-    from: `GetMentored <${process.env.EMAIL_ADDRESS}>`,
-    to: option.email,
-    subject: 'Reset GetMentored.com Password',
-    text: option.message
+    transporter.sendMail(message, function (error, info) {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('Email sent: ' + info.response)
+      }
+    })
+  } catch (err) {
+    console.error('Email sent failed: ', err)
   }
-  transporter.sendMail(message, function (error, info) {
-    if (error) {
-      console.log(error)
-    } else {
-      console.log('Email sent: ' + info.response)
-    }
-  })
 }
 
 module.exports = sendEmail
